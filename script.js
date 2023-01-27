e7c35a9ec3fd772765dceae6b36a4fae460955f6
ChristopherM8 <71267962+123cmorales@users.noreply.github.com>
2023-01-27 14:46:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var moon = document.getElementById("myDIV");
+  if (moon.style.display === "none") {
+    moon.style.display = "block";
+  } else {
+    moon.style.display = "none";
+  }
+}
